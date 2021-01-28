ccb6a3d201a5eb91a231e8783049666e748e2031
Patri2020 <patricia_miller@yahoo.com>
2021-01-28 02:06:51 -0600

    ...

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+    public class Main {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+	    // write your code here
+            int myAreaCode = 512;
+            int myNumber = 5555555;
+            String number = "telephone number";
+
+            System.out.println("My " + number+" is " +"("+myAreaCode+")"+ myNumber);
+            System.out.println(myAreaCode+" is an integer " + "and " + number+ " is a string in java syntax");
+
+            String classTTS = "Java";
+            String classInstructor = "Nick & Lionel";
+            int classSize = 40;
+            int classDate = 01/2021;
+            Date classDateRange = 01/2021-03/2021;
+
+            System.out.println(classTTS+" with "+classInstructor+" with a class size of "+classSize+" during"+Date);
+
+        }
+    }
